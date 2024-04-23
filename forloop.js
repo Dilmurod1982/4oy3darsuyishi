a11055eb9fc762ae7fa4913ad929571c3971aed1
cipher <dilsurbet@hmail.com>
2024-04-23 10:12:04 +0500

    forloop

---
+++ forloop.js
@@ -0,0 +1,44 @@
+// // 1 vazifa
+
+// for (let i = 0; i < n; i++) {
+//     console.log(n)
+// }
+
+// // 2 vazifa
+
+// for (let i = n; i < 1; i++) {
+//     console.log(n)
+// }
+
+// 3 vazifa
+// let jami = 0
+// for (let i = 1; i < 10; i++) {
+//     jami +=i;
+// }
+
+// console.log(jami)
+
+// 4 vazifa
+
+// let jami = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         evenSum += i;
+//     }
+// }
+// console.log(jami);
+
+//  5 vazifa
+
+let jami = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+        jami += i;
+    }
+}
+console.log(jami);
+
